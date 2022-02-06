1e246b92888331e4dca71ad33ac3d443eb6feb38
MagsudA <maqsud@live.com>
2022-02-06 10:54:18 -0500

    Test from Intellij

---
+++ src/bean/test3.java
@@ -0,0 +1,38 @@
+package bean;
+
+import com.sun.jdi.PathSearchingVirtualMachine;
+
+import java.util.Arrays;
+
+public class test3 {
+    public static void main(String[] args) {
+        System.out.println("Salam Necesen.\n Sagol yaxsiyam");
+        System.out.println("Salam Necesen.\t Sagol yaxsiyam");
+
+        System.out.println(056);
+
+        float sum;
+
+        sum = 16791+5;
+        System.out.println(sum);
+
+        System.out.println("\\");
+        System.out.println("\"Maqsud\"");
+
+        System.out.println("                    max");
+        System.out.println();
+        System.out.println();
+
+    }
+
+
+}
+
+// Todo: - create Todays package : Test3
+
+/* Todo: - created first line Todo here....
+This is secodn line
+This is third line
+ */
+
+
